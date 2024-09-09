00d36afaf012c2dc9b480ca795e93d5d3b69ae03
rjm <robjmass@gmail.com>
2024-09-09 15:04:18 -0400

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -38,23 +38,22 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1 style={{ alignSelf: "center" }}>Chemulator</h1>
-                    <input
-                        style={{ width: `${Math.max(inputValue.length, 10)}ch`, fontSize: "xx-large", padding: "5px", alignSelf: "center" }}
-                        type="text"
-                        value={formula}
-                        onChange={handleInputChange}
-                        onKeyPress={handleKeyPress}
-                        placeholder="(e.g. C6H6)"
-                    />
+                <input
+                    style={{ width: `${Math.max(inputValue.length, 10)}ch`, fontSize: "xx-large", padding: "5px", alignSelf: "center" }}
+                    type="text"
+                    value={formula}
+                    onChange={handleInputChange}
+                    onKeyPress={handleKeyPress}
+                    placeholder="(e.g. C6H6)"
+                />
             </header>
             <div className='page'>
-                <div className="structure">
-                    {molSVG ? (
-                        <div style={{ transform: "scale(4)" }}dangerouslySetInnerHTML={{ __html: molSVG }} />
-                    ) : (
-                            <p style={{ fontSize: "x-large", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>Enter a valid chemical formula in SMILES notation to begin.</p>
-                        )}
-                </div>
+                <div className='page-header'>{formula}</div>
+                {molSVG ? (
+                    <div style={{ transform: "scale(4)" }}dangerouslySetInnerHTML={{ __html: molSVG }} />
+                ) : (
+                        <p style={{ fontSize: "x-large", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>Enter a valid chemical formula in SMILES notation to begin.</p>
+                    )}
 
             </div>
         </div>
