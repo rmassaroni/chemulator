da69c1f63190ad94e649be96de1a9d65a16db9c1
rjm <robjmass@gmail.com>
2024-09-09 13:16:08 -0400

    cloned electron-react-ts template repo

---
+++ README.md
@@ -0,0 +1,7 @@
+React, Typescrpt, & Electron app template
+
+```
+sudo chown root:root ~/projects/electron-react-ts/node_modules/electron/dist/chrome-sandbox
+sudo chmod 4755 ~/projects/electron-react-ts/node_modules/electron/dist/chrome-sandbox
+```
+
---
+++ electron.js
@@ -0,0 +1,79 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+async function createWindow() {
+  const isDev = (await import('electron-is-dev')).default;
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, 'build/index.html')}`
+  );
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// const { app, BrowserWindow } = require('electron');
+// const path = require('path');
+// const isDev = require('electron-is-dev');
+//
+// function createWindow() {
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true,
+//       contextIsolation: false,
+//     },
+//   });
+//
+//   mainWindow.loadURL(
+//     isDev
+//       ? 'http://localhost:3000'
+//       : `file://${path.join(__dirname, '../build/index.html')}`
+//   );
+//
+//   if (isDev) {
+//     mainWindow.webContents.openDevTools();
+//   }
+// }
+//
+// app.on('ready', createWindow);
+//
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+//
+// app.on('activate', () => {
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     createWindow();
+//   }
+// });
+//
